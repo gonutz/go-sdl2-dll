a9b25c88feef3b9bbed88c99104d2fb283bf2477
lars <lars.scheme@gmail.com>
2019-11-06 20:49:49 +0100

    Update DLLs to version 2.0.10

--- sdl/test_32_bit.bat
+++ sdl/test_32_bit.bat
@@ -1,4 +1,4 @@
 set GOOS=windows
 set GOARCH=386
-go test -args SDL2-2_0_9-386.dll
+go test -args SDL2-2_0_10-386.dll
 if errorlevel 1 pause
--- sdl/test_64_bit.bat
+++ sdl/test_64_bit.bat
@@ -1,4 +1,4 @@
 set GOOS=windows
 set GOARCH=amd64
-go test -args SDL2-2_0_9-amd64.dll
+go test -args SDL2-2_0_10-amd64.dll
 if errorlevel 1 pause
